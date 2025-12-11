89894517dfe5128919baa807e00b47eb131e9612
jeongyoon <86715383+yunzz-park@users.noreply.github.com>
2025-12-11 22:16:44 +0900

    폼 구현

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function PostDetail() {
   return (
     <>
@@ -14,7 +16,9 @@ export default function PostDetail() {
           <div className="post__title"></div>
           <div className="post__utils-box">
             <div className="post__delete">삭제</div>
-            <div className="post__edit">수정</div>
+            <div className="post__edit">
+              <Link to={`/posts/edit/1`}>수정</Link>
+            </div>
           </div>
           <div className="post__text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
